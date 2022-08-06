cc94bea9f35cf7774489df618d33bc95cb41fccc
lvfd <lyufudi@yahoo.com>
2022-08-06 22:41:51 +0800

    Add PM2

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # doveUiServer
+# lvfd@acca.com.cn
+
+# Install pm2 to global enviroment before run this project.
--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ const express = require('express')
 const app = express()
 const port = 3013
 
+console.log(`NODE_ENV = ${app.get('env')}`)
+
 // for uikit:
 app.use('/doveuikit', require('./router-uikit'))
-
+// console.log(`NODE_ENV = ${app.get('env')}`)
 // for webpack:
 // app.use('/doveutils', webpack)
 
